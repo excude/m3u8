acec942fdd45fc10f77db88c268830055397e394
excude <excude>
2023-02-10 09:58:58 +0900

    m3u8 Commit

---
+++ pl.php
@@ -0,0 +1,46 @@
+#EXTM3U x-tvg-url="http://m3u-ip.tv/demo-epg.php"
+#EXTINF:-1 tvg-id="QVC2Germany.de" status="timeout" group-title="Shopping",QVC Plus
+https://d2mn03dhv5o3g8.cloudfront.net/live/qvcde_plus_clean/bitrate1.isml/3/prog_index.m3u8
+#EXTINF:-1 tvg-id="QVCGermany.de" status="timeout" group-title="Shopping",QVC Germany
+https://d2mn03dhv5o3g8.cloudfront.net/live/qvcde_main_clean/bitrate1.isml/3/prog_index.m3u8
+#EXTINF:-1 tvg-id="MotorvisionTV.de" status="timeout",Motorvision TV (720p)
+https://stream.ads.ottera.tv/playlist.m3u8?network_id=535
+#EXTINF:-1 tvg-id="HSE24Extra.de" status="timeout",HSE24 Extra (1080p)
+https://hse24extra.akamaized.net/hls/live/2006596/hse24extra/playlist.m3u8
+#EXTINF:-1 tvg-id="HRFernsehen.de" status="timeout",hr-fernsehen (1080p)
+https://hrhls.akamaized.net/hls/live/2024525/hrhls/index.m3u8
+#EXTINF:-1 tvg-id="RakutenTVComedyMoviesAustria.at" status="online",Rakuten Comedy Movies Austria
+#EXTGRP:Rakuten
+https://rakuten-comedymovies-5-eu.rakuten.wurl.tv/playlist.m3u8
+#EXTINF:-1 tvg-id="RakutenTVComedyMoviesGermany.de" status="online",Rakuten Comedy Movies Germany
+#EXTGRP:Rakuten
+https://rakuten-comedymovies-3-eu.rakuten.wurl.tv/playlist.m3u8
+#EXTINF:-1 tvg-id="RakutenTVDramaAustria.at" status="online",Rakuten TV Shows Austria
+#EXTGRP:Rakuten
+https://rakuten-tvshows-5-eu.rakuten.wurl.tv/playlist.m3u8
+#EXTINF:-1 tvg-id="RakutenTVActionMoviesGermany.de" status="online",Rakuten Action Movies Germany
+#EXTGRP:Rakuten
+https://rakuten-actionmovies-3-eu.rakuten.wurl.tv/playlist.m3u8
+#EXTINF:-1 tvg-id="RakutenTVDramaGermany.de" status="online",Rakuten TV Shows Germany
+#EXTGRP:Rakuten
+https://rakuten-tvshows-3-eu.rakuten.wurl.tv/playlist.m3u8
+#EXTINF:-1 tvg-id="RakutenTVSpotlightGermany.de" status="online",Rakuten Spotlight Germany
+#EXTGRP:Rakuten
+https://rakuten-spotlight-3-eu.rakuten.wurl.tv/playlist.m3u8
+#EXTINF:-1 tvg-id="RakutenTVTopFreeGermany.de" status="online",Rakuten Top Free Germany
+#EXTGRP:Rakuten
+https://rakuten-topfree-3-eu.rakuten.wurl.tv/playlist.m3u8
+#EXTINF:-1 tvg-id="EuronewsGerman.fr" status="timeout",Euronews German
+https://rakuten-euronews-5-de.samsung.wurl.com/manifest/playlist.m3u8
+#EXTINF:0 tvg-country="JP" tvg-logo="https://i.imgur.com/9u9TaMA.png" group-title="Shopping",QVC Japan
+https://cdn-live1.qvc.jp/iPhone/1501/1501.m3u8?checkedby:iptvcat.com
+#EXTINF:-1 tvg-id="BumblebeeTVLetsPlayMinecraft.us" tvg-logo="https://i.imgur.com/EqvWDU4.jpg" group-title="Kids",Bumblebee TV Lets Play Minecraft
+https://stitcheraws.unreel.me/wse-node02.powr.com/live/5e2625700145130f25474624/playlist.m3u8
+#EXTINF:-1 tvg-id="BumblebeeTVMasterBuilder.us" tvg-logo="https://i.imgur.com/KQsAdB4.png" group-title="Kids",Bumblebee TV Master Builder
+https://stitcheraws.unreel.me/wse-node02.powr.com/live/5bf2256ed5eeee0f5a4094bb/playlist.m3u8
+#EXTINF:-1 tvg-id="BumblebeeTVToyZone.us" tvg-logo="https://i.imgur.com/uRxkOD1.jpg" group-title="Kids",Bumblebee TV Toy Zone
+https://stitcheraws.unreel.me/wse-node02.powr.com/live/5bf22491932c8304fc4533e4/playlist.m3u8
+#EXTINF:-1 tvg-id="DasErste.de" status="timeout" group-title="GEZ?",Das Erste (720p)
+https://derste247liveint.akamaized.net/hls/live/662735/daserste_int/master.m3u8
+#EXTINF:-1 tvg-id="FEBC" group-title="Radio",ZDF info
+https://mlive2.febc.net/live/seoulam/playlist.m3u8
